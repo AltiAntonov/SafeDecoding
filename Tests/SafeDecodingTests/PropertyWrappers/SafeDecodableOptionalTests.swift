51af349a614feec2f0a7c8c985eac3354d5b6eb2
Altimir Antonov <altimir.antonov@gmail.com>
2026-04-27 20:06:39 +0300

    test: cover nested wrapper decoding

--- Tests/SafeDecodingTests/PropertyWrappers/SafeDecodableOptionalTests.swift
+++ Tests/SafeDecodingTests/PropertyWrappers/SafeDecodableOptionalTests.swift
@@ -6,6 +6,11 @@ private enum OptionalSuiteUnknownRoleFallback: SafeDecodingFallbackProvider {
     static let fallbackValue = "Unknown"
 }
 
+private enum Visibility: String, Decodable {
+    case `public`
+    case `private`
+}
+
 @Test
 func safeFieldDecodesValidString() throws {
     let data = #"{"name":"Ava"}"#.data(using: .utf8)!
@@ -49,6 +54,23 @@ private struct MixedWrapperUser: Decodable {
     @SafeFallbackDecodable<OptionalSuiteUnknownRoleFallback> var role: String
 }
 
+private struct NestedProfile: Decodable {
+    @SafeDecodable var name: String?
+}
+
+private struct NestedUser: Decodable {
+    let id: Int
+    let profile: NestedProfile
+}
+
+private struct NestedPreferences: Decodable {
+    @SafeDecodable var visibility: Visibility?
+}
+
+private struct NestedPreferencesUser: Decodable {
+    let preferences: NestedPreferences
+}
+
 @Test
 func brokenOptionalFieldStillFallsBackToNilAlongsideFallbackBackedField() throws {
     let data = #"{"id":1,"name":42,"role":42}"#.data(using: .utf8)!
@@ -58,4 +80,23 @@ func brokenOptionalFieldStillFallsBackToNilAlongsideFallbackBackedField() throws
     #expect(user.id == 1)
     #expect(user.name == nil)
     #expect(user.role == "Unknown")
+}
+
+@Test
+func nestedBrokenSafeFieldDoesNotFailWholeModel() throws {
+    let data = #"{"id":1,"profile":{"name":42}}"#.data(using: .utf8)!
+
+    let user = try JSONDecoder().decode(NestedUser.self, from: data)
+
+    #expect(user.id == 1)
+    #expect(user.profile.name == nil)
+}
+
+@Test
+func nestedBrokenWrappedEnumFallsBackToNil() throws {
+    let data = #"{"preferences":{"visibility":"friends-only"}}"#.data(using: .utf8)!
+
+    let user = try JSONDecoder().decode(NestedPreferencesUser.self, from: data)
+
+    #expect(user.preferences.visibility == nil)
 }
